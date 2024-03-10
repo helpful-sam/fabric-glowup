006b64260df7cb9c21f6e2352e5a0b3bf40ca48b
Sam <harrow48625@gmail.com>
2024-03-10 15:40:53 +0900

    initial gradle build failed: no minecraft-server.jar

---
+++ src/client/java/dev/fabric/glow/GlowingoresClient.java
@@ -0,0 +1,10 @@
+package dev.fabric.glow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlowingoresClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/dev/fabric/glow/GlowingoresDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.fabric.glow;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GlowingoresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
